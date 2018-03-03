c311c1f2977eb64f46884a4c6462a9552c4aa51e
Vijay-Nirmal <pon.vijaynirmal@outlook.com>
2018-03-04 00:09:58 +0530

    Major update
    - Added scrollIfVisibile,  additionalHorizontalOffset, additionalVerticalOffset
    - Improved UI

--- SmoothScrollingHelper/Helpers.cs
+++ SmoothScrollingHelper/Helpers.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.UI.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,36 +16,30 @@ namespace SmoothScrollingHelper
 {
     public static class Helpers
     {
-        public static ScrollViewer GetScrollViewer(this DependencyObject element)
+        /// <summary>
+        /// Smooth scrolling the list to bring the specified index into view 
+        /// </summary>
+        /// <param name="listViewBase">List to scroll</param>
+        /// <param name="index">The intex to bring into view</param>
+        /// <param name="itemPlacement">Set the item placement after scrolling</param>
+        /// <param name="disableAnimation">Set true to disable animation</param>
+        /// <param name="scrollIfVisibile">Set true to disable scrolling when the corresponding item is in view</param>
+        /// <param name="additionalHorizontalOffset">Adds additional horizontal offset</param>
+        /// <param name="additionalVerticalOffset">Adds additional vertical offset</param>
+        /// <returns>Note: Even though this return <see cref="Task"/>, it will not wait until the scrolling completes</returns>
+        public static async Task SmoothScrollIntoViewWithIndex(this Windows.UI.Xaml.Controls.ListViewBase listViewBase, int index, ItemPlacement itemPlacement = ItemPlacement.Default, bool disableAnimation = false, bool scrollIfVisibile = true, int additionalHorizontalOffset = 0, int additionalVerticalOffset = 0)
         {
-            if (element is ScrollViewer)
+            if (Math.Abs(index) > listViewBase.Items.Count)
             {
-                return (ScrollViewer)element;
+                throw new IndexOutOfRangeException("Index can't be greater than number of items in " + listViewBase.GetType().Name);
             }
 
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
-            {
-                var child = VisualTreeHelper.GetChild(element, i);
+            index = (index < 0) ? (index + listViewBase.Items.Count) : index;
 
-                var result = GetScrollViewer(child);
-                if (result == null)
-                {
-                    continue;
-                }
-                else
-                {
-                    return result;
-                }
-            }
-            return null;
-        }
-
-        public static async Task SmoothScrollIntoViewWithIndex(this ListViewBase listViewBase, int index, ItemPosition itemPosition = ItemPosition.Default, bool disableAnimation = false)
-        {
             bool isVirtualizing = default(bool);
             double previousXOffset = default(double), previousYOffset = default(double);
 
-            var scrollViewer = listViewBase.GetScrollViewer();
+            var scrollViewer = listViewBase.FindDescendant<ScrollViewer>();
             var selectorItem = listViewBase.ContainerFromIndex(index) as SelectorItem;
 
             if (selectorItem == null)
@@ -54,7 +51,8 @@ namespace SmoothScrollingHelper
 
                 var tcs = new TaskCompletionSource<object>();
 
-                EventHandler<ScrollViewerViewChangedEventArgs> viewChanged = (s, e) => tcs.TrySetResult(null);
+                void viewChanged(object s, ScrollViewerViewChangedEventArgs e) => tcs.TrySetResult(null);
+
                 try
                 {
                     scrollViewer.ViewChanged += viewChanged;
@@ -76,7 +74,8 @@ namespace SmoothScrollingHelper
             {
                 var tcs = new TaskCompletionSource<object>();
 
-                EventHandler<ScrollViewerViewChangedEventArgs> viewChanged = (s, e) => tcs.TrySetResult(null);
+                void viewChanged(object s, ScrollViewerViewChangedEventArgs e) => tcs.TrySetResult(null);
+
                 try
                 {
                     scrollViewer.ViewChanged += viewChanged;
@@ -93,60 +92,112 @@ namespace SmoothScrollingHelper
             var selectorItemWidth = selectorItem.ActualWidth;
             var listViewBaseHeight = listViewBase.ActualHeight;
             var selectorItemHeight = selectorItem.ActualHeight;
+
             previousXOffset = scrollViewer.HorizontalOffset;
             previousYOffset = scrollViewer.VerticalOffset;
 
-            if (itemPosition == ItemPosition.Left)
-            {
-                scrollViewer.ChangeView(position.X, previousYOffset, null, disableAnimation);
-            }
-            else if (itemPosition == ItemPosition.Top)
-            {
-                scrollViewer.ChangeView(previousXOffset, position.Y, null, disableAnimation);
-            }
-            else if (itemPosition == ItemPosition.Centre)
-            {
-                var CentreX = (listViewBaseWidth - selectorItemWidth) / 2.0;
-                var CentreY = (listViewBaseHeight - selectorItemHeight) / 2.0;
-                var finalXPosition = position.X - CentreX;
-                var finalYPosition = position.Y - CentreY;
-                scrollViewer.ChangeView(finalXPosition, finalYPosition, null, disableAnimation);
-            }
-            else if (itemPosition == ItemPosition.Right)
-            {
-                var finalXPosition = position.X - listViewBaseWidth + selectorItemWidth;
-                scrollViewer.ChangeView(finalXPosition, previousYOffset, null, disableAnimation);
-            }
-            else if (itemPosition == ItemPosition.Bottom)
-            {
-                var finalYPosition = position.Y - listViewBaseHeight + selectorItemHeight;
-                scrollViewer.ChangeView(previousXOffset, finalYPosition, null, disableAnimation);
-            }
-            else if (itemPosition == ItemPosition.Default)
-            {
-                var bottomXPosition = position.X - listViewBaseWidth + selectorItemWidth;
-                var finalXPosition = position.X;
+            var minXPosition = position.X - listViewBaseWidth + selectorItemWidth;
+            var minYPosition = position.Y - listViewBaseHeight + selectorItemHeight;
 
-                if (previousXOffset < position.X && previousXOffset > bottomXPosition)
-                    finalXPosition = previousXOffset;
-                else if (Math.Abs(previousXOffset - bottomXPosition) < Math.Abs(previousXOffset - position.X))
-                    finalXPosition = bottomXPosition;
+            var maxXPosition = position.X;
+            var maxYPosition = position.Y;
 
-                var rightYPosition = position.Y - listViewBaseHeight + selectorItemHeight;
-                var finalYPosition = position.Y;
+            double finalXPosition, finalYPosition;
 
-                if (previousYOffset < position.Y && previousYOffset > rightYPosition)
-                    finalYPosition = previousYOffset;
-                else if (Math.Abs(previousYOffset - rightYPosition) < Math.Abs(previousYOffset - position.Y))
-                    finalYPosition = rightYPosition;
-
-                scrollViewer.ChangeView(finalXPosition, finalYPosition, null, disableAnimation);
+            if (!scrollIfVisibile && (previousXOffset <= maxXPosition && previousXOffset >= minXPosition) && (previousYOffset <= maxYPosition && previousYOffset >= minYPosition))
+            {
+                finalXPosition = previousXOffset;
+                finalYPosition = previousYOffset;
             }
+            else
+            {
+                switch (itemPlacement)
+                {
+                    case ItemPlacement.Default:
+                        if (previousXOffset <= maxXPosition && previousXOffset >= minXPosition)
+                        {
+                            finalXPosition = previousXOffset + additionalHorizontalOffset;
+                        }
+                        else if (Math.Abs(previousXOffset - minXPosition) < Math.Abs(previousXOffset - maxXPosition))
+                        {
+                            finalXPosition = minXPosition + additionalHorizontalOffset;
+                        }
+                        else
+                        {
+                            finalXPosition = maxXPosition + additionalHorizontalOffset;
+                        }
+
+                        if (previousYOffset <= maxYPosition && previousYOffset >= minYPosition)
+                        {
+                            finalYPosition = previousYOffset + additionalVerticalOffset;
+                        }
+                        else if (Math.Abs(previousYOffset - minYPosition) < Math.Abs(previousYOffset - maxYPosition))
+                        {
+                            finalYPosition = minYPosition + additionalVerticalOffset;
+                        }
+                        else
+                        {
+                            finalYPosition = maxYPosition + additionalVerticalOffset;
+                        }
+
+                        break;
+
+                    case ItemPlacement.Left:
+                        finalXPosition = maxXPosition + additionalHorizontalOffset;
+                        finalYPosition = previousYOffset + additionalVerticalOffset;
+                        break;
+
+                    case ItemPlacement.Top:
+                        finalXPosition = previousXOffset + additionalHorizontalOffset;
+                        finalYPosition = maxYPosition + additionalVerticalOffset;
+                        break;
+
+                    case ItemPlacement.Centre:
+                        var centreX = (listViewBaseWidth - selectorItemWidth) / 2.0;
+                        var centreY = (listViewBaseHeight - selectorItemHeight) / 2.0;
+                        finalXPosition = maxXPosition - centreX + additionalHorizontalOffset;
+                        finalYPosition = maxYPosition - centreY + additionalVerticalOffset;
+                        break;
+
+                    case ItemPlacement.Right:
+                        finalXPosition = minXPosition + additionalHorizontalOffset;
+                        finalYPosition = previousYOffset + additionalVerticalOffset;
+                        break;
+
+                    case ItemPlacement.Bottom:
+                        finalXPosition = previousXOffset + additionalHorizontalOffset;
+                        finalYPosition = minYPosition + additionalVerticalOffset;
+                        break;
+
+                    default:
+                        finalXPosition = previousXOffset + additionalHorizontalOffset;
+                        finalYPosition = previousYOffset + additionalVerticalOffset;
+                        break;
+                }
+            }
+
+            scrollViewer.ChangeView(finalXPosition, finalYPosition, null, disableAnimation);
         }
 
-        public static void SmoothScrollNavigation(this ListViewBase listViewBase, int scrollAmount, ScrollNavigationDirection scrollNavigationDirection, bool disableAnimation = false)
+        /// <summary>
+        /// Smooth scrolling the list to bring the specified data item into view 
+        /// </summary>
+        /// <param name="listViewBase">List to scroll</param>
+        /// <param name="index">The data item to bring into view</param>
+        /// <param name="itemPlacement">Set the item placement after scrolling</param>
+        /// <param name="disableAnimation">Set true to disable animation</param>
+        /// <param name="ScrollIfVisibile">Set true to disable scrolling when the corresponding item is in view</param>
+        /// <param name="additionalHorizontalOffset">Adds additional horizontal offset</param>
+        /// <param name="additionalVerticalOffset">Adds additional vertical offset</param>
+        /// <returns>Note: Even though this return <see cref="Task"/>, it will not wait until the scrolling completes</returns>
+        public static async Task SmoothScrollIntoViewWithItem(this Windows.UI.Xaml.Controls.ListViewBase listViewBase, object item, ItemPlacement itemPlacement = ItemPlacement.Default, bool disableAnimation = false, bool scrollIfVisibile = true, int additionalHorizontalOffset = 0, int additionalVerticalOffset = 0)
         {
-            var scrollViewer = listViewBase.GetScrollViewer();
+            await SmoothScrollIntoViewWithIndex(listViewBase, listViewBase.Items.IndexOf(item), itemPlacement, disableAnimation, scrollIfVisibile, additionalHorizontalOffset, additionalVerticalOffset);
+        }
+
+        public static void SmoothScrollNavigation(this Windows.UI.Xaml.Controls.ListViewBase listViewBase, int scrollAmount, ScrollNavigationDirection scrollNavigationDirection, bool disableAnimation = false)
+        {
+            var scrollViewer = listViewBase.FindDescendant<ScrollViewer>();
 
             if (scrollNavigationDirection == ScrollNavigationDirection.Left)
             {
@@ -170,7 +221,7 @@ namespace SmoothScrollingHelper
     /// <summary>
     /// Item Position
     /// </summary>
-    public enum ItemPosition
+    public enum ItemPlacement
     {
         /// <summary>
         /// If visible then it will not scroll, if not then item will be aligned to the nearest edge
--- SmoothScrollingHelper/MainPage.xaml.cs
+++ SmoothScrollingHelper/MainPage.xaml.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Media;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -16,8 +17,7 @@ namespace SmoothScrollingHelper
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private ObservableCollection<ItemSource> ItemsSourceCollection = new ObservableCollection<ItemSource>();
-        int intex = 0;
+        private ObservableCollection<int> ItemsSourceCollection = new ObservableCollection<int>();
 
         public MainPage()
         {
@@ -25,29 +25,24 @@ namespace SmoothScrollingHelper
 
             for (int i = 0; i < 800; i++)
             {
-                ItemsSourceCollection.Add(new ItemSource { Name = i.ToString() });
+                ItemsSourceCollection.Add(i);
             }
 
-            CurrentIntex.Text = "CurrentIntex - " + intex.ToString();
+            var enumValue = Enum.GetValues(typeof(ItemPlacement)).Cast<ItemPlacement>();
+            ItemPlacementComboBox.ItemsSource = enumValue.ToList();
+            ItemPlacementComboBox.SelectedItem = ItemPlacement.Default;
         }
 
-        private void GoToButton_Click(object sender, RoutedEventArgs e)
+        private async void GoToButton_Click(object sender, RoutedEventArgs e)
         {
-            if (int.Parse(GoToIntex.Text) >= 0 && int.Parse(GoToIntex.Text) < MyGridView.Items.Count)
-            {
-                MyGridView.SelectedIndex = int.Parse(GoToIntex.Text);
-            }
-        }
+            int.TryParse(GoToIntex.Text, out int goToIntex);
+            ItemPlacement itemPlacement = (ItemPlacement)ItemPlacementComboBox.SelectedItem;
+            bool disableAnimation = DisableAnimationToggleSwitch.IsOn;
+            bool scrollIfVisibile = ScrollIfVisibileToggleSwitch.IsOn;
+            int.TryParse(AdditionalHorizontalOffsetTextBox.Text, out int additionalHorizontalOffset);
+            int.TryParse(AdditionalHorizontalOffsetTextBox.Text, out int additionalVerticalOffset);
 
-        private async void MyGridView_SelectionChangedAsync(object sender, SelectionChangedEventArgs e)
-        {
-            if (MyGridView.SelectedItem == null)
-                return;
-
-            intex = MyGridView.SelectedIndex;
-            CurrentIntex.Text = "CurrentIntex - " + intex.ToString();
-            await MyGridView.SmoothScrollIntoViewWithIndex(MyGridView.SelectedIndex, ItemPosition.Default);
-
+            await MyGridView.SmoothScrollIntoViewWithIndex(goToIntex, itemPlacement, disableAnimation, scrollIfVisibile, additionalHorizontalOffset, additionalVerticalOffset);
         }
 
         private void LeftButton_Click(object sender, RoutedEventArgs e)
@@ -69,12 +64,5 @@ namespace SmoothScrollingHelper
         {
             MyGridView.SmoothScrollNavigation(105, ScrollNavigationDirection.Down);
         }
-
     }
-
-    public class ItemSource
-    {
-        public string Name { get; set; }
-    }
-
 }
